0a0ebbf3b02f543e048032ea462ae7879d2fe37a
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 21:44:46 -0500

    Update Reto04.cs

--- Reto04.cs
+++ Reto04.cs
@@ -1,3 +1,9 @@
+/*
+Escribe un programa que pida al usuario ingrese su animal favorito, si coloca ‘Tortuga’, 
+‘tortuga’, ‘TORTUGA’ o cualquier otra variante de la palabra entonces mostrar en pantalla 
+“También me gustan las tortugas”. En caso contrario mostrar el mensaje “Ese animal es 
+genial, pero prefiero las tortugas”.
+*/
 using System;
 
 namespace tortuga
